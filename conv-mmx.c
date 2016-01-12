b7a209c82a436611abad536ea36ee9a0fe0a80b1
Yassine El Khadiri <yassine.el-khadiri@ensimag.grenoble-inp.fr>
2016-01-12 01:25:02 +0100

    add comment and check for rgb computation

--- conv-mmx.c
+++ conv-mmx.c
@@ -29,9 +29,8 @@
 #include <unistd.h>
 #include <stdint.h>
 
-#define UNROLL 4
 #define EXPAND(x) ((x)<<48|(x)<<32|(x)<<16|(x))
-#define YCrCb_DEBUG
+#define RGB_DEBUG
 
 /* SIMD optimized integer version with 4 iterations unrolled */
 void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, uint32_t nb_MCU_V)
@@ -51,7 +50,7 @@ void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, 
    MCU_Cb = YCrCb_MCU[1];
 
    for (i = 0; i < 8 * nb_MCU_V; i++) {
-      for (j = 0; j < 8 * nb_MCU_H; j += UNROLL) {
+      for (j = 0; j < 8 * nb_MCU_H; j += 4) {
          index = i * (8 * nb_MCU_H) + j;
          /* Set mm0 & mm7 to full zero */
          __asm__("pxor      %mm0, %mm0");
@@ -76,13 +75,13 @@ void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, 
 			val = MCU_Cr[index+3]<<24|MCU_Cr[index+2]<<16|MCU_Cr[index+1]<<8|MCU_Cr[index];
 			__asm__("movd %0, %%mm1"::"m"(val)); // mm1 := 0 0 0 0 Cr3 Cr2 Cr1 Cr0
 			__asm__("punpcklbw %mm7, %mm1"); // mm1 := 0 Cr3 0 Cr2 0 Cr1 0 Cr0
-         __asm__("psubw     %mm3, %mm1");
+         __asm__("psubw     %mm3, %mm1"); // mm1 := 0 Cr3-128 0 Cr2-128 0 Cr1-128 0 Cr0-128
 
          /* Load MCU_Cb into mm2 and expand it to 16 bits, and sub 128 */
 			val = MCU_Cb[index+3]<<24|MCU_Cb[index+2]<<16|MCU_Cb[index+1]<<8|MCU_Cb[index];
 			__asm__("movd %0, %%mm2"::"m"(val)); // mm1 := 0 0 0 0 Cb3 Cb2 Cb1 Cb0
 			__asm__("punpcklbw %mm7, %mm2"); // mm1 := 0 Cb3 0 Cb2 0 Cb1 0 Cb0
-         __asm__("psubw     %mm3, %mm2");
+         __asm__("psubw     %mm3, %mm2"); // mm1 := 0 Cb3-128 0 Cb2-128 0 Cb1-128 0 Cb0-128
 
 #ifdef YCrCb_DEBUG
          /* Checking the intermediate results */
@@ -98,69 +97,68 @@ void YCrCb_to_ARGB(uint8_t *YCrCb_MCU[3], uint32_t *RGB_MCU, uint32_t nb_MCU_H, 
 
 #endif
 
-         /* register containing 0 */
-         //__asm__("pxor      %mm6, %mm6");
-
          /* Here we have the Y_i in mm0, Cr_i in mm1 and Cb_i in mm2.
           * Since these values are used for R, G and B computations,
           * these registers should not be the target of any instruction */
 
          /* compute R in mm3 */
-			/*
+			//*
          uint64_t cst_359 = EXPAND((uint64_t)359);
-         __asm__("movq %mm1, %mm3");
-         __asm__("pmullw %0, %%mm3"::"m"(cst_359));
-         __asm__("paddw %mm0, %mm3");
-         __asm__("psrlw $8, %mm3");
-         __asm__("packuswb %mm6, %mm3");
+         __asm__("movq %mm1, %mm3");                   // mm3 := (Cr_i-128)
+         __asm__("pmullw %0, %%mm3"::"m"(cst_359));    // mm3 := (Cr_i-128)*359
+         __asm__("paddw %mm0, %mm3");                  // mm3 := (Y_i<<8) + (Cb_i-128)*359
+         __asm__("psrlw $8, %mm3");                    // mm3 := ((Y_i<<8) + (Cb_i-128)*359) >> 8
+         __asm__("packuswb %mm7, %mm3");               // mm3 := 0 0 0 0 R3 R2 R1 R0
 			//*/
 
 #ifdef RGB_DEBUG
-         uint8_t R[4];
-         __asm__("movd %%mm3, %0":"=m"(R[0]));
+         uint8_t R[8];
+         __asm__("movq %%mm3, %0":"=m"(R[0]));
+			printf("R = %02i %02i %02i %02i %02i %02i %02i %02i\n", R[7], R[6], R[5], R[4], R[3], R[2], R[1], R[0]);
 #endif
 
          /* compute G in mm4 */
 
          uint64_t cst_183 = EXPAND((uint64_t)183);
          /* use mm5 as intermediate result */
-			/*
-         __asm__("movq %mm1, %mm5");
-         __asm__("pmullw %0, %%mm5"::"m"(cst_183));
-         __asm__("movq %mm0, %mm4");
-         __asm__("psubw %mm5, %mm4");
+			//*
+         __asm__("movq %mm1, %mm5");                  // mm5 := (Cr_i-128)
+         __asm__("pmullw %0, %%mm5"::"m"(cst_183));   // mm5 := (Cr_i-128)*183
+         __asm__("movq %mm0, %mm4");                  // mm4 := (Y_i<<8)
+         __asm__("psubw %mm5, %mm4");                 // mm4 := (Y_i<<8) - (Cr_i-128)*183
 			//*/
 
          uint64_t cst_88 = EXPAND((uint64_t)88);
-			/*
-         __asm__("movq %mm2, %mm5");
-         __asm__("pmullw %0, %%mm5"::"m"(cst_88));
-         __asm__("psubw %mm5, %mm4");
-
-         __asm__("psrlw $8, %mm4");
-         __asm__("packuswb %mm6, %mm4");
+			//*
+         __asm__("movq %mm2, %mm5");                  // mm5 := (Cb_i-128)
+         __asm__("pmullw %0, %%mm5"::"m"(cst_88));    // mm5 := (Cb_i-128)*88
+         __asm__("psubw %mm5, %mm4");                 // mm4 := (Y_i<<8) - (Cr_i-128)*183 - (Cb_i-128)*88
+         __asm__("psrlw $8, %mm4");                   // mm4 := ((Y_i<<8) - (Cr_i-128)*183 - (Cb_i-128)*88) >> 8
+         __asm__("packuswb %mm6, %mm4");              // mm4 := 0 0 0 0 G3 G2 G1 G0
 			//*/
 
 #ifdef RGB_DEBUG
-         uint8_t G[4];
-         __asm__("movd %%mm4, %0":"=m"(G[0]));
+   		uint8_t G[8];
+         __asm__("movq %%mm3, %0":"=m"(G[0]));
+			printf("G = %02i %02i %02i %02i %02i %02i %02i %02i\n", G[7], G[6], G[5], G[4], G[3], G[2], G[1], G[0]);
 #endif
 
          /* compute B in mm5 */
 
+			//*
          uint64_t cst_455 = EXPAND((uint64_t)455);
-			/*
-         __asm__("movq %mm2, %mm5");
-         __asm__("pmullw %0, %%mm5"::"m"(cst_455));
-         __asm__("paddw %mm0, %mm5");
-         __asm__("psrlw $8, %mm5");
-         __asm__("packuswb %mm6, %mm5");
+         __asm__("movq %mm2, %mm5");                   // mm5 := (Cb_i-128)
+         __asm__("pmullw %0, %%mm5"::"m"(cst_455));    // mm5 := (Cb_i-128)*359
+         __asm__("paddw %mm0, %mm5");                  // mm5 := (Y_i<<8) + (Cb_i-128)*455
+         __asm__("psrlw $8, %mm5");                    // mm5 := ((Y_i<<8) + (Cb_i-128)*455) >> 8
+         __asm__("packuswb %mm7, %mm5");               // mm5 := 0 0 0 0 B3 B2 B1 B0
 			//*/
 
 
 #ifdef RGB_DEBUG
-         uint8_t B[4];
-         __asm__("movd      %%mm5, %0":"=m"(B[0]));
+    		uint8_t B[8];
+         __asm__("movq %%mm3, %0":"=m"(B[0]));
+			printf("B = %02i %02i %02i %02i %02i %02i %02i %02i\n", B[7], B[6], B[5], B[4], B[3], B[2], B[1], B[0]);
 #endif
          /* TODO: Now the 4 lowest bytes of mm3, mm4 and mm5 contain the 
           * 4 R, G and B values  of the iteration.
